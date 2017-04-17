d3629c64cd82181f56d9ecacd516c0a5c6931080
BarredGalaxy <BarredGalaxy@users.noreply.github.com>
2017-04-17 13:53:47 -0700

    Update verif.js

--- verif.js
+++ verif.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "*";
-const fs = require("fs");
+// const fs = require("fs");
 const color = [2012323, 4918289, 3482998, 0830827, 1273867, 7217478, 1276488];
 const rtd = ["Galactic Hole", "Holactic Gale", "DEUSOOP VULT", "lOWOlmemes", "I’ll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda.", "nick show", "garfield", "dank memes", "__can bonk the honk__", "Shitposter", "*livestream!*", "Holactic Gale", "Enlightenment", "'Dad'", "i really cant think of anything to put in this array of answers", "HonkStream", "memes", "this one is impossible"];
+const slap = ["https://media.giphy.com/media/y6dyjA5vmkUgw/giphy.gif", "http://p.fod4.com/p/media/87b82bdcd4/dU5uwWV3RqucQ7InODGZ_Third%20Hand%20Slap.gif", "https://media.giphy.com/media/D4K9yv0L8tqP6/giphy.gif", "http://gifsec.com/wp-content/uploads/GIF/2014/03/Slow-motion-slap-GIF.gif", "http://www.gifimagesdownload.com/wp-content/uploads/2016/02/latest-slap-gif-781.gif"];
 /*
 var express = require('express');
 var app     = express();
@@ -25,26 +26,63 @@ client.on('ready', () => {
   client.user.setUsername('HonkBot')
   console.log(`Logged in as ${client.user.username}!`);
   client.user.setGame("HonkBot: *help | I'm Currently in " + client.guilds.size + " Servers! | NEW MONTHLY CHALLENGES COMING SOON!")
-   
+  client.channels.get('275777565124198400').send("**Monthly Challenges:** Who ever makes the __BEST__ honkbot logo will get __**400 t! Credits & 1 REP**__ (More info: *monthly)");
 });
  
 client.on('message', msg => {
   if(msg.author.bot) return;
   if (msg.content.startsWith(prefix + "slap")) {
     let [username] = msg.content.split(" ").slice(1);
-    msg.channel.sendMessage(username + ' was slapped by ' + msg.author)
-  }
-  if (msg.content.includes("trump")) {
-      msg.react('👎');
-      msg.reply('Please dont discuss that horrible man. Ever. Like Ever. At all. Thx ;)')
+    msg.channel.sendMessage(username + ' was slapped by ' + msg.author + '\n' + slap[Math.floor(Math.random() * slap.length)])
   }
   if (msg.content.startsWith(prefix + "asl")) {
     let [age, sex, location] = msg.content.split(" ").slice(1);
     msg.reply(`Hello ${msg.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
   }
+  if (msg.content.startsWith(prefix + 'bot'))
+    msg.channel.sendMessage(" ", {embed: {
+            color: color[Math.floor(Math.random() * color.length)],
+            author: {
+                 name: "Bot Info",
+                icon_url: ""
+            },
+            description: "HonkBot : Written By @BarredGalaxy#6811",
+            fields: [
+                {
+                    inline: true,
+                    name: "Prefix",
+                    value: prefix
+                },
+                {
+                    inline: true,
+                    name: "Uptime",
+                    value: [Math.floor((client.uptime / 1000) / 60)] + "m"
+                },
+                {
+                    inline: true,
+                    name: "Servers",
+                    value: client.guilds.size
+                },
+                {
+                    inline: true,
+                    name: "Version",
+                    value: "1.93.23"
+                },
+                {
+                    inline: true,
+                    name: "Dev Server",
+                    value: "http://bit.ly/2oNP6hv"
+                },
+                {
+                    inline: true,
+                    name: "Server Ping",
+                    value: Math.round(msg.client.ping) + "ms"
+                }
+            ]
+        }});
   if (msg.content.startsWith(prefix + 'botstats'))
     msg.channel.sendMessage(" ", {embed: {
-            color: 7013413,
+            color: color[Math.floor(Math.random() * color.length)],
             author: {
                  name: "Bot Statistics",
                 icon_url: ""
@@ -82,20 +120,23 @@ client.on('message', msg => {
             fields: [
             ]
         }});
- 
-      const role = msg.guild.roles.find('name', 'Verified');
-      msg.member.addRole(role);
   if (msg.content.startsWith(prefix + 'ping')) {
     msg.reply('Test Pong is Done!');
   } 
   if (msg.content.startsWith(prefix + 'ty')) {
-    msg.reply('ty is the best he is very cool!! :D');
+    msg.reply('ty is the best he is very cool!! :D http://barred.ga/i/yes.png');
   } 
-  if (msg.content.startsWith(prefix + 'honk')) {
-    msg.reply('honk honk honk honk honk honk honk honk. its my name. and main purpose in life. to honk endlessly. by why i ask: a question i often find myself at. why? why do i honk forr just the entertainment of a buch a humans? just for a higher life forms pleasure? why do i honk on end. this is the eternal question. who am i? where am i? what am i? why am i? im just a robot. just a silly automaton. just for the sheer laughs of humans on a discord server.');
+  if (msg.content.startsWith(prefix + 'monthly')) {
+    msg.reply('**Monthly Challenges:** Who ever makes the __BEST__ honkbot logo will get __**400 t! Credits & 1 REP**__ //\n\n `MORE INFO: For questions, Ask @BarredGalaxy#6811, The Creator`');
   } 
+  if (msg.content.startsWith(prefix + 'upgrademe')) {
+    if (!msg.author.id == '241330918722109441') return;
+    msg.reply('yes');
+    const role = msg.guild.roles.find('name', 'beep boop');
+    msg.member.addRole(role);
+  }
   if (msg.content.startsWith(prefix + 'nick')) {
-    msg.reply('https://i.imgur.com/bDLWaEB.png');
+    msg.reply('We all love nick http://barred.ga/i/nick.png');
   } 
   if (msg.content.startsWith(prefix + "help")) {
     msg.channel.sendMessage("", {embed: {
@@ -156,39 +197,6 @@ client.on('message', msg => {
   }
      
 });
+
  
-// Verification Points
-let points = JSON.parse(fs.readFileSync('./points.json', 'utf8'));
-client.on("message", msg => {
-  if(msg.author.bot) return;
-  if (msg.content.startsWith(prefix + 'verify')) {
-    let userData = points[msg.author.id];
-    if(userData.points > 20) { 
-      let role = msg.guild.roles.find('name', 'Verified');
-      console.log(role);
-      msg.member.addRole(role);
-      msg.reply('You Got *Verified*!')
-    } else{
-      msg.reply('You Can Get *Verified,* But i dont want to.')
-    }
-  }
-  if (msg.content.includes('(╯°□°）╯︵ ┻━┻')) {
-    msg.channel.sendMessage('┬─┬ノ( ゜-゜ノ) this table is gonna break soon')
-  }
-  if(!points[msg.author.id]) points[msg.author.id] = {points: 0, level: 0, flip: 0};
-  let userData = points[msg.author.id];
-  userData.points++;
- 
-  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));  
-  if(curLevel > userData.points) {
-    userData.level = curLevel;
-    msg.reply(`You've leveled up to level **${curLevel}**! You should chat more`);
-  }
-  if(msg.content.startsWith(prefix + "points")) {
-    msg.channel.sendMessage("Your Balance is **" + userData.points + "**. Keep up the good work!");
-  }
- 
-  fs.writeFile('./points.json', JSON.stringify(points), (err) => {if(err) console.error(err)});
-});
- 
-client.login('wow ethan great moves keep it up.');
+client.login('Mjk3ODMyNzI0NjA4MDU3MzQ0.C9TeJQ.WK7-25lWqKn7Q6OugAEM6NYqGAM');
